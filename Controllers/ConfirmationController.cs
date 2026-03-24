b64c330a2a49388b59fa115fe3ff45538be7779f
BLVCKNESSS <yan.tayouo22@gmail.com>
2026-03-24 15:35:33 +0100

    modif tayouo

---
+++ Controllers/ConfirmationController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CyberTrack.DAL;
+using CyberTrack.Models.ViewModels;
+
+namespace CyberTrack.Controllers
+{
+    public class ConfirmationController : Controller
+    {
+        private readonly SignalementDAO _signalementDAO;
+
+        public ConfirmationController()
+        {
+            _signalementDAO = new SignalementDAO();
+        }
+
+        public ActionResult Index()
+        {
+            string ticket = TempData["Ticket"] as string;
+            TempData.Keep("Ticket");
+
+            if (string.IsNullOrWhiteSpace(ticket))
+            {
+                ticket = "SGN-2025-04827";
+            }
+
+            ConfirmationViewModel model = _signalementDAO.GetByTicket(ticket);
+
+            if (model == null)
+            {
+                return View("TicketIntrouvable");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Print(string ticket)
+        {
+            if (string.IsNullOrWhiteSpace(ticket))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ConfirmationViewModel model = _signalementDAO.GetByTicket(ticket);
+
+            if (model == null)
+            {
+                return View("TicketIntrouvable");
+            }
+
+            return View(model);
+        }
+    }
+}
